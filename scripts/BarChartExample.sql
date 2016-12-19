70a45c82b138fd9d10506d9003de7d9317b49091
bstabler <benstabler@yahoo.com>
2016-12-19 12:02:46 -0800

    additional clean-up

--- scripts/BarChartExample.sql
+++ scripts/BarChartExample.sql
@@ -6,10 +6,10 @@
 ALTER USER [ATLANTAREGION\TAMConsult] WITH DEFAULT_SCHEMA = BS10
 
 /* Count activity patterns by person type and return in BARGROUP, COLUMNS, QUANTITY format for Bar Chart visual */
-SELECT PERSON_TYPE AS "Person Group", 
+SELECT PERSON_TYPE AS "PERSON GROUP", 
   REPLACE(REPLACE(REPLACE(ACTIVITY_PATTERN, 'M' collate Latin1_General_CS_AS ,'Mandatory'), 
   'H' collate Latin1_General_CS_AS, 'Home'), 
-  'N' collate Latin1_General_CS_AS, 'Non-mandatory') AS "Day Pattern", 
+  'N' collate Latin1_General_CS_AS, 'Non-mandatory') AS "DAY PATTERN", 
   COUNT(ACTIVITY_PATTERN) AS "Count"
 FROM PERSONDATA 
 GROUP BY PERSON_TYPE, ACTIVITY_PATTERN 
