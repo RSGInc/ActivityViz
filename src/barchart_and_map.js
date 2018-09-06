8e6fb33c6aea9c9514f57deeee917366b45a3d7c
medailey <medaileyjr@gmail.com>
2018-09-06 11:35:12 -0400

    Bug Fixes
    Fixed bug in chord.js and barchart_and_map.js that offset the zones in the map by 1.
    
    Added label size option to region.json and edited chord.js to include font-size loaded.

--- src/barchart_and_map.js
+++ src/barchart_and_map.js
@@ -555,7 +555,7 @@ var barchart_and_map = (function () {
 			//create circle markers for each zone centroid
 			for (var i = 0; i < zoneTiles.features.length; i++) {
 				var feature = zoneTiles.features[i];
-				var featureZoneData = zoneData[feature.properties.id];
+				var featureZoneData = zoneData[feature.properties.id-1];
 				if (featureZoneData == undefined) { //missing data for this zone
 				} else {
 					//WARNING: center coordinates seem to have lat and lng reversed!
